03cc666233d5c4bc8d4c3f4c69db463b355d3731
a4desh-kumar <75736240+a4desh-kumar@users.noreply.github.com>
2021-01-19 03:47:31 +0500

    Update Network.h

--- Network.h
+++ Network.h
@@ -7,16 +7,18 @@ using namespace std;
 
 class Network
 {
-	vector<vector<Neuron>> layers;    
+	vector<vector<Neuron>> layers;
+	double learning_rate;
+	int epoch_lim;
 public:
 	Network(vector<int> layers);  // Every index represents layer and value represents total number of neurons in that layer
-	void set_output_values(const vector<double> &input);
+	void forward_feed(const vector<double> &input, bool a, int y=-1);
+	void back_propagation(const vector<double> &expected);
 	void printOutput();
-	double derivative_of_activation_function(double value);
+	double derivative(double value);
+	double activation(double value);
 	double calculate_error(vector<double>& expected);
 };
 
 
 #endif // !NETWORK
-
-
