9eca20435256714fa06a425782aa36eb4e3e3abe
a4desh-kumar <75736240+a4desh-kumar@users.noreply.github.com>
2021-01-19 03:48:29 +0500

    Update Neuron.h

--- Neuron.h
+++ Neuron.h
@@ -7,19 +7,18 @@ using namespace std;
 
 class Neuron
 {
-	vector<double> weight;                    // Weight from one node to next layer's node
-	vector<double> change_in_weight;
+	vector<double> weights;                    // Weight from one node to next layer's node
 	double output_val;
+	double bias;
+	double activation;
 public:
 	Neuron(int prev_layer_neurons=0);
 	static double get_random_weight();
 	double get_output_val();
 	double get_weight(int index);
 	void set_output_val(double output_val);
+	friend class Network;
 };
 
 
 #endif // !NEURON
-
-
-
